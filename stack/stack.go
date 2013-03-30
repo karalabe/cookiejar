9887c8aecd2228d9f6fe0b7b696d74e63890bb11
Péter Szilágyi <peterke@gmail.com>
2013-03-30 15:25:50 +0100

    Stack implementation and tests.

---
+++ stack/stack.go
@@ -0,0 +1,83 @@
+// CookieJar - A contestant's algorithm toolbox
+// Copyright 2013 Peter Szilagyi. All rights reserved.
+//
+// CookieJar is dual licensed: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// The toolbox is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+// more details.
+//
+// Alternatively, the CookieJar toolbox may be used in accordance with the terms
+// and conditions contained in a signed written agreement between you and the
+// author(s).
+//
+// Author: peterke@gmail.com (Peter Szilagyi)
+
+// Package stack implements a LIFO (last in first out) data structure supporting
+// arbitrary types (even a mixture).
+//
+// Internally it uses a slice of blocks, resulting in faster resizing than a
+// simple dynamic array/slice would allow.
+package stack
+
+// The size of a block of data
+const blockSize = 4096
+
+type Stack struct {
+	size     int
+	capacity int
+	offset   int
+
+	blocks [][]interface{}
+	active []interface{}
+}
+
+// Creates a new, empty stack.
+func New() *Stack {
+	result := new(Stack)
+	result.active = make([]interface{}, blockSize)
+	result.blocks = [][]interface{}{result.active}
+	result.capacity = blockSize
+	return result
+}
+
+// Pushes a value onto the stack, expanding it if necessary.
+func (s *Stack) Push(data interface{}) {
+	if s.size == s.capacity {
+		s.active = make([]interface{}, blockSize)
+		s.blocks = append(s.blocks, s.active)
+		s.capacity += blockSize
+		s.offset = 0
+	}
+	s.active[s.offset] = data
+	s.offset++
+	s.size++
+}
+
+// Pops a value off the stack and returns it. Currently no shrinking is done.
+func (s *Stack) Pop() interface{} {
+	s.size--
+	s.offset--
+	if s.offset < 0 {
+		s.offset = blockSize - 1
+		s.active = s.blocks[s.size/blockSize]
+	}
+	return s.active[s.offset]
+}
+
+// Checks whether the stack is empty or not.
+func (s *Stack) Empty() bool {
+	return s.size == 0
+}
+
+// Resets the stack, effectively clearing its contents.
+func (s *Stack) Reset() {
+	s.size = 0
+	s.offset = 0
+	s.blocks = [][]interface{}{s.active}
+	s.capacity = blockSize
+}
---
+++ stack/stack_test.go
@@ -0,0 +1,93 @@
+// CookieJar - A contestant's algorithm toolbox
+// Copyright 2013 Peter Szilagyi. All rights reserved.
+//
+// CookieJar is dual licensed: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// The toolbox is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+// more details.
+//
+// Alternatively, the CookieJar toolbox may be used in accordance with the terms
+// and conditions contained in a signed written agreement between you and the
+// author(s).
+//
+// Author: peterke@gmail.com (Peter Szilagyi)
+
+// Package stack implements a LIFO (last in first out) data structure supporting
+// arbitrary types (even a mixture).
+//
+// Internally it uses a slice of blocks, resulting in faster resizing than a
+// simple dynamic array/slice would allow.
+package stack
+
+import (
+	"math/rand"
+	"testing"
+)
+
+func TestStack(t *testing.T) {
+	// Create some initial data
+	size := 1048576
+	data := make([]int, size)
+	for i := 0; i < size; i++ {
+		data[i] = rand.Int()
+	}
+	// Push all the data into the stack, pop out every second
+	secs := []int{}
+	stack := New()
+	for i := 0; i < size; i++ {
+		stack.Push(data[i])
+		if i%2 == 0 {
+			secs = append(secs, stack.Pop().(int))
+		}
+	}
+	rest := []int{}
+	for !stack.Empty() {
+		rest = append(rest, stack.Pop().(int))
+	}
+	// Make sure the contents of the resulting slices are ok
+	for i := 0; i < size; i++ {
+		if i%2 == 0 && data[i] != secs[i/2] {
+			t.Errorf("push/pop mismatch: have %v, want %v.", secs[i/2], data[i])
+		}
+		if i%2 == 1 && data[i] != rest[len(rest)-i/2-1] {
+			t.Errorf("push/pop mismatch: have %v, want %v.", rest[len(rest)-i/2-1], data[i])
+		}
+	}
+}
+
+func TestReset(t *testing.T) {
+	// Push some stuff onto the stack
+	size := 1048576
+	stack := New()
+	for i := 0; i < size; i++ {
+		stack.Push(i)
+	}
+	// Clear and verify
+	stack.Reset()
+	if !stack.Empty() {
+		t.Errorf("stack not empty after reset: %v", stack)
+	}
+}
+
+func BenchmarkPush(b *testing.B) {
+	stack := New()
+	for i := 0; i < b.N; i++ {
+		stack.Push(i)
+	}
+}
+
+func BenchmarkPop(b *testing.B) {
+	stack := New()
+	for i := 0; i < b.N; i++ {
+		stack.Push(i)
+	}
+	b.ResetTimer()
+	for !stack.Empty() {
+		stack.Pop()
+	}
+}
