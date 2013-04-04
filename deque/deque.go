6525580fe8a17d580d1e065934c1a836a58b5673
Péter Szilágyi <peterke@gmail.com>
2013-04-04 23:08:22 +0200

    Deque implementation + tests.

---
+++ deque/deque.go
@@ -0,0 +1,143 @@
+// CookieJar - A contestant's algorithm toolbox
+// Copyright (c) 2013 Peter Szilagyi. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright notice,
+//       this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+// Alternatively, the CookieJar toolbox may be used in accordance with the terms
+// and conditions contained in a signed written agreement between you and the
+// author(s).
+//
+// Author: peterke@gmail.com (Peter Szilagyi)
+
+// Package deque implements a double ended queue supporting arbitrary types
+// (even a mixture).
+//
+// Internally it uses a dynamically growing circular slice of blocks, resulting
+// in faster resizes than a simple dynamic array/slice would allow, yet less gc
+// overhead.
+package deque
+
+// The size of a block of data
+const blockSize = 4096
+
+// Double ended queue data structure.
+type Deque struct {
+	leftIdx  int
+	leftOff  int
+	rightIdx int
+	rightOff int
+
+	blocks [][]interface{}
+	left   []interface{}
+	right  []interface{}
+}
+
+// Creates a new, empty deque.
+func New() *Deque {
+	result := new(Deque)
+	result.blocks = [][]interface{}{make([]interface{}, blockSize)}
+	result.right = result.blocks[0]
+	result.left = result.blocks[0]
+	return result
+}
+
+// Pushes a new element into the queue from the right, expanding it if necessary.
+func (d *Deque) PushRight(data interface{}) {
+	d.right[d.rightOff] = data
+	d.rightOff++
+	if d.rightOff == blockSize {
+		d.rightOff = 0
+		d.rightIdx = (d.rightIdx + 1) % len(d.blocks)
+
+		// If we wrapped over to the left, insert a new block and update indices
+		if d.rightIdx == d.leftIdx {
+			buffer := make([][]interface{}, len(d.blocks)+1)
+			copy(buffer[:d.rightIdx], d.blocks[:d.rightIdx])
+			buffer[d.rightIdx] = make([]interface{}, blockSize)
+			copy(buffer[d.rightIdx+1:], d.blocks[d.rightIdx:])
+			d.blocks = buffer
+			d.leftIdx++
+			d.left = d.blocks[d.leftIdx]
+		}
+		d.right = d.blocks[d.rightIdx]
+	}
+}
+
+// Pops out an element from the queue from the right. Note, no bounds checking are done.
+func (d *Deque) PopRight() interface{} {
+	d.rightOff--
+	if d.rightOff < 0 {
+		d.rightOff = blockSize - 1
+		d.rightIdx = (d.rightIdx - 1 + len(d.blocks)) % len(d.blocks)
+		d.right = d.blocks[d.rightIdx]
+	}
+	return d.right[d.rightOff]
+}
+
+// Pushes a new element into the queue from the left, expanding it if necessary.
+func (d *Deque) PushLeft(data interface{}) {
+	d.leftOff--
+	if d.leftOff < 0 {
+		d.leftOff = blockSize - 1
+		d.leftIdx = (d.leftIdx - 1 + len(d.blocks)) % len(d.blocks)
+
+		// If we wrapped over to the right, insert a new block and update indices
+		if d.leftIdx == d.rightIdx {
+			d.leftIdx++
+			buffer := make([][]interface{}, len(d.blocks)+1)
+			copy(buffer[:d.leftIdx], d.blocks[:d.leftIdx])
+			buffer[d.leftIdx] = make([]interface{}, blockSize)
+			copy(buffer[d.leftIdx+1:], d.blocks[d.leftIdx:])
+			d.blocks = buffer
+		}
+		d.left = d.blocks[d.leftIdx]
+	}
+	d.left[d.leftOff] = data
+}
+
+// Pops out an element from the queue from the left. Note, no bounds checking are done.
+func (d *Deque) PopLeft() interface{} {
+	res := d.left[d.leftOff]
+	d.leftOff++
+	if d.leftOff == blockSize {
+		d.leftOff = 0
+		d.leftIdx = (d.leftIdx + 1) % len(d.blocks)
+		d.left = d.blocks[d.leftIdx]
+	}
+	return res
+}
+
+// Checks whether the queue is empty.
+func (d *Deque) Empty() bool {
+	return d.leftIdx == d.rightIdx && d.leftOff == d.rightOff
+}
+
+// Clears out the contents of the queue.
+func (d *Deque) Reset() {
+	d.leftIdx = 0
+	d.rightIdx = 0
+	d.leftOff = 0
+	d.rightOff = 0
+	d.blocks = [][]interface{}{make([]interface{}, blockSize)}
+	d.left = d.blocks[0]
+	d.right = d.blocks[0]
+}
---
+++ deque/deque_test.go
@@ -0,0 +1,127 @@
+// CookieJar - A contestant's algorithm toolbox
+// Copyright (c) 2013 Peter Szilagyi. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright notice,
+//       this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+// Alternatively, the CookieJar toolbox may be used in accordance with the terms
+// and conditions contained in a signed written agreement between you and the
+// author(s).
+//
+// Author: peterke@gmail.com (Peter Szilagyi)
+package deque
+
+import (
+	"math/rand"
+	"testing"
+)
+
+func TestDeque(t *testing.T) {
+	// Create some initial data
+	size := 1048576
+	data := make([]int, size)
+	for i := 0; i < size; i++ {
+		data[i] = rand.Int()
+	}
+	deque := New()
+	for rep := 0; rep < 2; rep++ {
+		// Push all the data into the deque, each one on a different side
+		outs := []int{}
+		for i := 0; i < size; i++ {
+			if i%2 == 0 {
+				deque.PushLeft(data[i])
+			} else {
+				deque.PushRight(data[i])
+			}
+			// Pop out every third and fourth (inversely than inserted)
+			if i%4 == 2 {
+				outs = append(outs, deque.PopRight().(int))
+			} else if i%4 == 3 {
+				outs = append(outs, deque.PopLeft().(int))
+			}
+		}
+		rest := []int{}
+		for !deque.Empty() {
+			if len(rest)%2 == 0 {
+				rest = append(rest, deque.PopRight().(int))
+			} else {
+				rest = append(rest, deque.PopLeft().(int))
+			}
+		}
+		// Make sure the contents of the resulting slices are ok
+		for i := 1; i < size; i += 4 {
+			if data[i] != outs[i/2] {
+				t.Errorf("push/pop mismatch: have %v, want %v.", outs[i/2], data[i])
+			}
+			if data[i+1] != outs[i/2+1] {
+				t.Errorf("push/pop mismatch: have %v, want %v.", outs[i/2+1], data[i+1])
+			}
+		}
+		for i := 0; i < size; i += 4 {
+			if data[i] != rest[len(rest)-1-i/2] {
+				t.Errorf("push/pop mismatch: have %v, want %v.", rest[len(rest)-1-i/2], data[i])
+			}
+			if data[i+3] != rest[len(rest)-1-i/2-1] {
+				t.Errorf("push/pop mismatch: have %v, want %v.", rest[len(rest)-1-i/2-1], data[i+1])
+			}
+		}
+	}
+}
+
+func TestReset(t *testing.T) {
+	// Push some stuff into the deque
+	size := 1048576
+	deque := New()
+	for i := 0; i < size; i++ {
+		deque.PushLeft(i)
+	}
+	// Clear and verify
+	deque.Reset()
+	if !deque.Empty() {
+		t.Errorf("deque not empty after reset: %v", deque)
+	}
+}
+
+func BenchmarkPush(b *testing.B) {
+	deque := New()
+	for i := 0; i < b.N; i++ {
+		if i%2 == 0 {
+			deque.PushLeft(i)
+		} else {
+			deque.PushRight(i)
+		}
+	}
+}
+
+func BenchmarkPop(b *testing.B) {
+	deque := New()
+	for i := 0; i < b.N; i++ {
+		deque.PushLeft(i)
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		if i%2 == 0 {
+			deque.PopLeft()
+		} else {
+			deque.PopRight()
+		}
+	}
+}
